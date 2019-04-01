ca298fbc33b356fab3ec6436986c05ab393a79d3
SamueleAlpino <samuelealpino@gmail.com>
2019-04-01 19:54:51 +0200

    Load project

---
+++ WindowsFormsApp1/Window.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFolderButton = new System.Windows.Forms.Button();
+            this.Visualizer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenFolderButton
+            // 
+            this.OpenFolderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenFolderButton.Location = new System.Drawing.Point(26, 23);
+            this.OpenFolderButton.Name = "OpenFolderButton";
+            this.OpenFolderButton.Size = new System.Drawing.Size(195, 39);
+            this.OpenFolderButton.TabIndex = 0;
+            this.OpenFolderButton.Text = "SELECT FOLDER";
+            this.OpenFolderButton.UseVisualStyleBackColor = true;
+            this.OpenFolderButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Visualizer
+            // 
+            this.Visualizer.BackColor = System.Drawing.Color.LightGray;
+            this.Visualizer.Font = new System.Drawing.Font("Miriam CLM", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Visualizer.Location = new System.Drawing.Point(29, 72);
+            this.Visualizer.Margin = new System.Windows.Forms.Padding(0);
+            this.Visualizer.Name = "Visualizer";
+            this.Visualizer.Size = new System.Drawing.Size(193, 23);
+            this.Visualizer.TabIndex = 1;
+            this.Visualizer.Text = "Temporary";
+            this.Visualizer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ORDER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(243, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Visualizer);
+            this.Controls.Add(this.OpenFolderButton);
+            this.Name = "Window";
+            this.Text = "Organizer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFolderButton;
+        private System.Windows.Forms.Label Visualizer;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Window.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Window : Form
+    {
+        private FolderBrowserDialog dialog = new FolderBrowserDialog();
+        private string directory;
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                directory       = dialog.SelectedPath;
+                Visualizer.Text = directory;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DivideAccordingToExtension();
+        }
+        //For each extension if there are almost two files of each type, create a folder and move them in
+        private List<string> SelectAllFilesInFolder(string folder)
+        {
+            List<string> toReturn = null;
+
+            if (Directory.Exists(folder))
+               toReturn = Directory.GetFiles(folder).ToList();
+
+            return toReturn;
+        }
+
+        private void DivideAccordingToExtension()
+        {
+            List<string> extensions = new List<string>();
+            List<string> allFiles = SelectAllFilesInFolder(directory);
+           
+            //Get all extensione type
+            foreach (string file in allFiles)
+            {
+                string ext = Path.GetExtension(file);
+
+                if (!extensions.Contains(ext))
+                    extensions.Add(ext);
+            }
+
+            List<DirectoryInfo> dir = new List<DirectoryInfo>();
+            //Create directory according the extensions
+            foreach (string ext in extensions)
+            {
+                //TODO: check if directory already exist
+                DirectoryInfo d =  Directory.CreateDirectory(Path.Combine(directory,ext));
+                dir.Add(d);
+            }
+
+            //Do private in class
+            string currentExt;
+            foreach (DirectoryInfo folder in dir)
+            {
+                foreach (string file in allFiles)
+                {
+                    currentExt = Path.GetExtension(file);
+                    if (currentExt == folder.Name)
+                    {
+                        int index = file.LastIndexOf("\\") + 1;
+                        string name = file.Substring(index);
+                        File.Move(file,Path.Combine(directory,folder.Name,name));
+                    }
+                }
+            }
+
+            extensions.Clear();
+            allFiles.Clear();
+            dir.Clear();
+        }
+
+       
+    }
+}
+
+//string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+//string[] unwantedExtensions = { "exe", "txt", "ccp" }; // you can extend it  
+//StringCollection col = new StringCollection();  
+//foreach (string file in files )  
+//{  
+//   string ext = Path.GetExtension(file);  
+//   if ( ! unwantedExtensions.Contains<string>(ext ) )  
+//   {  
+//      col.Add(file );  
+//   }  
+//}  
